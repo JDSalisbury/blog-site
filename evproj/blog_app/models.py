6a586f0ad775b49c72443a378c96b31378ce3587
Jeffrey Salisbury <jsalisbury@everest.local>
2018-10-18 09:42:07 -0400

    created comment and post classes

--- evproj/blog_app/models.py
+++ evproj/blog_app/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
+
+class BlogPost(models.Model):
+    author = models.ForeignKey('auth.User', null=True, on_delete=SET_NULL)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True, null=True)
+
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.coments.filter(approved_comment=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(model.Model):
+    post = models.ForeignKey('blog_app.BlogPost', related_name='comments')
+    author = models.CharField(max_length=100)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
