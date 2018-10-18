24b0a8206b4a244c7fe720cd0e03de628ea08cf7
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-10-18 10:40:44 -0400

    added additional views and the urls that go with them

--- evproj/blog_app/views.py
+++ evproj/blog_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from blog_app.models import BlogPost, Comment
-from django.views.generic import (TemplateView, ListView)
+from blog_app.forms import PostForm, CommentForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (TemplateView, ListView, DetailView, CreateView)
 
 # Create your views here.
 
@@ -13,4 +16,11 @@ class PostListView(ListView):
     def get_queryset(self):
         return BlogPost.objects.filter(published_date__lte=timezone.now().order_by('-published_date')))
 
-    
+class PostDetailView(DetialView):
+    model = BlogPost
+
+class CreatePostView(LoginRequiredMixin,CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog_app/post_detail.html'
+    form_class = Postform
+    model = BlogPost
